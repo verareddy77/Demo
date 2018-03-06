79db2045574d6852626a2fb8760db378fe9c1e15
veerareddygvr <verareddy007@gmail.com>
2018-03-06 15:08:43 +0530

    will appear added

--- GithubDemo/ViewController.swift
+++ GithubDemo/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
     }
 
     override func didReceiveMemoryWarning() {
